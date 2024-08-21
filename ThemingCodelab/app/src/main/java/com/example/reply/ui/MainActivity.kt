dcc7228713823b136637f2e18f4044d41a019382
tomoya0x00 <to-miwa@unext.jp>
2024-08-21 10:45:18 +0900

    Add space key event problem reproducing code

--- ThemingCodelab/app/src/main/java/com/example/reply/ui/MainActivity.kt
+++ ThemingCodelab/app/src/main/java/com/example/reply/ui/MainActivity.kt
@@ -19,24 +19,41 @@ package com.example.reply.ui
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.reply.data.LocalEmailsDataProvider
 
 
 class MainActivity : ComponentActivity() {
 
-    private val viewModel: ReplyHomeViewModel by viewModels()
+    //private val viewModel: ReplyHomeViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
+            Button(
+                modifier = Modifier.onFocusEvent {
+                    Log.d(TAG, "focus event changed(hasFocus: ${it.hasFocus}, isFocused:${it.isFocused})")
+                },
+                onClick = {
+                    Log.d(TAG, "button is clicked")
+                },
+            ) {
+                Text("DUMMY BUTTON")
+            }
+            /*
             val uiState by viewModel.uiState.collectAsState()
             ReplyApp(
                 replyHomeUIState = uiState,
@@ -47,9 +64,27 @@ class MainActivity : ComponentActivity() {
                     viewModel.setSelectedEmail(emailId)
                 }
             )
+
+             */
         }
     }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        Log.d(TAG, "onKeyDown: keyCode=$keyCode, event=$event")
+
+        return super.onKeyDown(keyCode, event)
+    }
+
+    override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
+        Log.d(TAG, "onKeyUp: keyCode=$keyCode, event=$event")
+        return super.onKeyUp(keyCode, event)
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
 }
+
 
 @Preview(
     uiMode = UI_MODE_NIGHT_YES,
